4b25a31391bc9889fbe205fdabb708e8917f5c69
Joshy Chan <155045525+joshychan-bcps@users.noreply.github.com>
2025-06-13 16:49:23 -0500

    completely forgot

--- src/app/(login)/signout/server-actions.ts
+++ src/app/(login)/signout/server-actions.ts
@@ -1,28 +1,24 @@
 "use server";
-import { headers } from "next/headers";
-import { auth } from "~/server/auth";
 import { ory } from "~/utils/ory";
 
 export async function oryLogout(logoutChallenge: string, choice: boolean) {
-  const request = await ory.getOAuth2LogoutRequest({
-    logoutChallenge: logoutChallenge,
-  }).then((res) => res.data);
+  const request = await ory
+    .getOAuth2LogoutRequest({
+      logoutChallenge: logoutChallenge,
+    })
+    .then((res) => res.data);
   if (!choice) {
-    await ory
-      .rejectOAuth2LogoutRequest({
-        logoutChallenge: logoutChallenge,
-      });
-    return new URL(request.request_url!, "https://accounts.bloxvalschools.com").searchParams.get("post_logout_redirect_uri"); 
+    await ory.rejectOAuth2LogoutRequest({
+      logoutChallenge: logoutChallenge,
+    });
+    return new URL(
+      request.request_url!,
+      "https://accounts.bloxvalschools.com",
+    ).searchParams.get("post_logout_redirect_uri");
   } else {
-    const [oryLogoutAccept] = await Promise.all([
-      ory
-        .acceptOAuth2LogoutRequest({
-          logoutChallenge: logoutChallenge,
-        }),
-      auth.api.signOut({
-        headers: await headers(),
-      })
-    ]);
+    const oryLogoutAccept = await ory.acceptOAuth2LogoutRequest({
+      logoutChallenge: logoutChallenge,
+    });
 
     return oryLogoutAccept.data.redirect_to;
   }
