a09451fe2d27c01abf2bd5af307003989339070c
joshuadl12 <josh@dlftx.com>
2026-08-04 12:33:07 +0000

    make key generator also produce an RS256 key

--- keything.ts
+++ keything.ts
@@ -3,6 +3,19 @@ import { db } from "~/server/db";
 import { oauth2Keys } from "~/server/db/schema";
 
 
+void generateKeyPair("RS256", {
+  extractable: true,
+}).then(async (res) => {
+  const yes = {
+    id: crypto.randomUUID(),
+    alg: "RS256",
+    public_key: await exportJWK(res.publicKey),
+    private_key: await exportJWK(res.privateKey),
+  }
+  console.log(await db.insert(oauth2Keys).values(yes).returning());
+});
+
+
 void generateKeyPair("RS512", {
   extractable: true,
 }).then(async (res) => {
