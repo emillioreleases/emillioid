4662707184119d6f0b641447e56ce0fb82e43918
joshuadl12 <josh@dlftx.com>
2026-08-07 20:42:53 +0000

    add roblox endpoint

--- src/app/(login)/signin/sso-buttons.tsx
+++ src/app/(login)/signin/sso-buttons.tsx
@@ -31,29 +31,31 @@ export default function SSOButtons({
           </div>
         </button>
       </Link>
-      <button
-        className="flex w-full border-t-2 border-r-2 border-b-2 border-l-2 border-t-white border-r-black border-b-black border-l-white bg-[#c3c3c3] active:border-t-4 active:border-t-black active:border-r-white active:border-b-white active:border-l-black"
-      >
-        <div
-          className={
-            "flex h-full w-full items-center justify-center space-x-2 border-r-2 border-b-2 border-r-[#838381] border-b-[#838381] p-3 text-black active:border-r-0 active:border-b-0 " +
-            W95Font.className
-          }
+      <Link href="/api/social/roblox/login" className="w-full">
+        <button
+          className="flex w-full border-t-2 border-r-2 border-b-2 border-l-2 border-t-white border-r-black border-b-black border-l-white bg-[#c3c3c3] active:border-t-4 active:border-t-black active:border-r-white active:border-b-white active:border-l-black"
         >
-          <Image
-            src={"/auth-logos/roblox.svg"}
-            alt={"Roblox Logo"}
-            width={25}
-            height={25}
-            style={{
-              filter: "invert(1)",
-            }}
-          />
-          <p>
-            Login with <span className={W95FontBold.className}>Roblox</span>
-          </p>
-        </div>
-      </button>
+          <div
+            className={
+              "flex h-full w-full items-center justify-center space-x-2 border-r-2 border-b-2 border-r-[#838381] border-b-[#838381] p-3 text-black active:border-r-0 active:border-b-0 " +
+              W95Font.className
+            }
+          >
+            <Image
+              src={"/auth-logos/roblox.svg"}
+              alt={"Roblox Logo"}
+              width={25}
+              height={25}
+              style={{
+                filter: "invert(1)",
+              }}
+            />
+            <p>
+              Login with <span className={W95FontBold.className}>Roblox</span>
+            </p>
+          </div>
+        </button>
+      </Link>
     </div>
   );
 }
