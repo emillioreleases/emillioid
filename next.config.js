6092473718121d094f2eff16f843f1a71fdd0e69
joshuadl12 <josh@dlftx.com>
2026-08-07 22:52:37 +0000

    remove cloudflare things

--- next.config.js
+++ next.config.js
@@ -2,11 +2,8 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
 import "./src/env.js";
-
-initOpenNextCloudflareForDev()
 
 /** @type {import("next").NextConfig} */
 const config = {
--- src/utils/cloudflare.ts
+++
@@ -1,3 +0,0 @@
-import { getCloudflareContext } from "@opennextjs/cloudflare";
-
-export const cfCtx = getCloudflareContext({ async: true });
